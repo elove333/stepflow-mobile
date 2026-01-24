a84fd0d0c38e18035eb597812a93b76696ccfe9a
Emerald love Hardee <elovehardee3@gmail.com>
2026-01-24 13:42:38 -0600

    chore: add minimal src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App: React.FC = () => (
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.text}>STEPFLOW</Text>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  text: { fontSize: 20 },
+});
+
+export default App;
